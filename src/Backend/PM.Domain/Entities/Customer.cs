5fa9ef8300a287b73e0831e1a63237af7bcbe4a8
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-09-01 17:51:50 -0300

    feat: adding a user, business and customer entities

---
+++ src/Backend/PM.Domain/Entities/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace PM.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public int BusinessId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? RG { get; set; }
+        public string? CPF { get; set; }
+
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
+        public string? District { get; set; }
+
+        public bool IsSmoker { get; set; } = false;
+        public string? Instagram { get; set; }
+        public string? VehiclePlate { get; set; }
+        public string? VehicleModel { get; set; }
+
+        public string? HowDiscoveryHotel { get; set; }
+    }
+}
---
+++ src/Backend/PM.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using PM.Domain.Enums;
+
+namespace PM.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        protected User() { }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public RoleType Role { get; set; } = RoleType.Customer;
+        public List<Business> Businesses { get; set; } = [];
+    }
+}
