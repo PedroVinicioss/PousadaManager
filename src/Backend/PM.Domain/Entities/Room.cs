01b39675f509fdd875aea468335193edf4276ba3
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-09-01 17:52:30 -0300

    feat: adding a host entities

---
+++ src/Backend/PM.Domain/Entities/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace PM.Domain.Entities
+{
+    public class Room : BaseEntity
+    {
+        public int BusinessId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
